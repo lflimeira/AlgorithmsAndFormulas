87312f46740f6c4ffd7f175401803744e97c3e02
Luiz Felipe de Oliva Limeira <lflimeira02@gmail.com>
2017-05-26 17:07:02 -0300

    Add function finished

--- binary-tree/tree.class.php
+++ binary-tree/tree.class.php
@@ -1,5 +1,5 @@
 <?php  
-
+include("node.class.php");
 /**
 * Binary tree  - Tree Class
 */
@@ -8,21 +8,38 @@ class Tree
 {
 	private $root;
 
-	function __construct()
-	{
-		$this->root = null;
+	function __construct($value)
+	{ 
+		echo "Binary tree example </br>";
+		$newNode = new Node($value);
+		$this->root = $newNode;
+		echo "Creating a binary tree with the root ".$this->root->getValue()." </br>";
 	}
 
-	public function add($value)
+	public function add($node, $value)
 	{
-
-		$newNode = new Node($value);
-		if ($this->root == null) {
-			$this->root = $newNode;
+		if($value < $node->getValue()){
+			if($node->getLeftSon() != null){
+				$this->add($node->getLeftSon(), $value);
+			}else{
+				echo " Inserting ".$valor." into the left of ".$node->getValue()." </br>";
+				$node->setLeftSon(new Node($value));
+			}
+		}else if($value > $node->getValue()){
+			if($node->getRightSon() != null){
+				$this->add($node->getRightSon(), $value);
+			}else{
+				echo " Inserting ".$valor." into the right of ".$node->getValue()." </br>";
+				$node->getRightSon(new Node($value));
+			}
 		}
+	}
 
-		if($value < $this->root->getValue()){
-			
+	public function remove($node, $value)
+	{
+		echo " Reading node ".$node->getValue()." </br>";
+		if($node == null){
+			echo " Tree empty </br>";
 		}
 	}
 }
