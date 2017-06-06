a7abb3a0a51d5e0e0c519a02ef96555410e38ed3
Luiz Felipe de Oliva Limeira <lflimeira02@gmail.com>
2017-06-06 17:20:35 -0300

    The main algorithm of the binary tree is finished.

--- binary-tree/index.php
+++ binary-tree/index.php
@@ -1,7 +1,24 @@
 <?php  
 
+include("tree.class.php");
+
 /**
 * Binary tree  
 */
+
+$tree = new Tree(20);
+$tree->add($tree->getRoot(), 22);
+$tree->add($tree->getRoot(), 6);
+$tree->add($tree->getRoot(), 15);
+$tree->add($tree->getRoot(), 8);
+$tree->add($tree->getRoot(), 17);
+$tree->add($tree->getRoot(), 7);
+$tree->add($tree->getRoot(), 3);
+$tree->add($tree->getRoot(), 11);
+$tree->add($tree->getRoot(), 9);
+$tree->add($tree->getRoot(), 16);
+$tree->add($tree->getRoot(), 1);
+$tree->remove($tree->getRoot(), 15);
+$tree->remove($tree->getRoot(), 1);
 
 ?>
--- binary-tree/tree.class.php
+++ binary-tree/tree.class.php
@@ -16,21 +16,26 @@ class Tree
 		echo "Creating a binary tree with the root ".$this->root->getValue()." </br>";
 	}
 
+	public function getRoot()
+	{
+		return $this->root;
+	}
+
 	public function add($node, $value)
 	{
 		if($value < $node->getValue()){
 			if($node->getLeftSon() != null){
 				$this->add($node->getLeftSon(), $value);
 			}else{
-				echo " Inserting ".$valor." into the left of ".$node->getValue()." </br>";
+				echo " Inserting ".$value." into the left of ".$node->getValue()." </br>";
 				$node->setLeftSon(new Node($value));
 			}
 		}else if($value > $node->getValue()){
 			if($node->getRightSon() != null){
 				$this->add($node->getRightSon(), $value);
 			}else{
-				echo " Inserting ".$valor." into the right of ".$node->getValue()." </br>";
-				$node->getRightSon(new Node($value));
+				echo " Inserting ".$value." into the right of ".$node->getValue()." </br>";
+				$node->setRightSon(new Node($value));
 			}
 		}
 	}
@@ -41,5 +46,44 @@ class Tree
 		if($node == null){
 			echo " Tree empty </br>";
 		}
+			
+		if ($value < $node->getValue()) {
+			$node->setLeftSon($this->remove($node->getLeftSon(), $value));
+		}
+		else if ($value > $node->getValue()) {
+			$node->setRightSon($this->remove($node->getRightSon(), $value));
+		} else if ($node->getLeftSon() != null && $node->getRightSon() != null) {
+			echo " Removed node ".$node->getValue()." </br>";
+			$node->setValue( $this->findMinimum($node->getRightSon())->getValue());
+			$node->setRightSon($this->removeMinimum($node->getRightSon()));
+		} else {
+			echo " Removed node ".$node->getValue()." </br>";
+			$node = ($node->getLeftSon() == null) ? $node->getLeftSon() : $node->getRightSon();
+		}
+
+		return $node;
+	}
+
+	public function removeMinimum($node)
+	{
+		if ($node == null) {
+			echo " ERROR ";
+		} else if ($node->getLeftSon() != null) {
+			$node->setLeftSon($this->removeMinimum($node->getLeftSon()));
+			return $node;
+		} else {
+			return $node->getRightSon();
+		}
+		return null;
+	}
+
+	public function findMinimum($node)
+	{
+		if ($node != null) {
+			while($node->getLeftSon() != null){
+				$node = $node->getLeftSon();
+			}
+		}
+		return $node;
 	}
 }
